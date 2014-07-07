da44e9f801ed0fe88149d8af73cda459fcfb60d7
qcsky <chaoqp@gmail.com>
2014-07-07 14:09:01 +0800

    fix plugin paths

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,8 +47,8 @@ DEFAULT_PAGINATION = 10
 THEME = 'pelican-theme/pelican-octopress-theme/'
 
 # Plugin
-PLUGIN_PATH = 'pelican-plugins/liquid_tags'
-PLUGINS = ['liquid_tags.img', 'liquid_tags.video', 'liquid_tags.code',
+PLUGIN_PATH = 'pelican-plugins'
+PLUGINS = ['liquid_tags.img', 'liquid_tags.video', 'liquid_tags.include_code',
 			'liquid_tags.literal'
 			]
 
